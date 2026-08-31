ce4274e2177f8ac630bb560af2ae4e6f742474c3
meirajessica75-bit <meirajessica75@gmail.com>
2026-08-31 18:53:13 -0300

    Update termoquimica_app.py

--- termoquimica_app.py
+++ termoquimica_app.py
@@ -371,8 +371,8 @@ elif st.session_state.etapa == "Vídeo de Apoio":
     """)
     
     # Vídeo embutido
-    st.video("https://www.youtube.com/watch?v=8mG5bJz82S0")
-    st.markdown("🔗 **[CLIQUE AQUI PARA ASSISTIR DIRETAMENTE NO YOUTUBE](https://www.youtube.com/watch?v=8mG5bJz82S0)** (caso o player integrado esteja bloqueado ou indisponível na rede da escola).")
+    st.video("https://youtu.be/5aPH2E9UxhM?si=SH952-T550RETmS8")
+    st.markdown("🔗 **[CLIQUE AQUI PARA ASSISTIR DIRETAMENTE NO YOUTUBE](https://youtu.be/5aPH2E9UxhM?si=SH952-T550RETmS8)** (caso o player integrado esteja bloqueado ou indisponível na rede da escola).")
     
     # Alternativa de leitura caso o vídeo esteja bloqueado ou indisponível
     st.markdown("---")
